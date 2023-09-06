2aa3f9129c5e5d7d3e4d4a36e8690c0000827fed
jrsr5599 <johnraab2002@yahoo.com>
2023-09-05 19:25:31 -0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -7,3 +7,37 @@ class User extends Model {
       return bcrypt.compareSync(loginPw, this.password);
     }
   }
+
+  // create fields/columns for User model
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    github: {
+      type: DataTypes.STRING,
+      allowNull: true
+  },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4]
+      }
+    }
+  })

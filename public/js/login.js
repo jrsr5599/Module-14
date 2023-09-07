a9cb3492ef0f0cf828301963dccd30c462994ff1
jrsr5599 <johnraab2002@yahoo.com>
2023-09-07 16:05:21 -0700

    Update login.js

---
+++ public/js/login.js
@@ -0,0 +1,6 @@
+const loginFormHandler = async function(event) {
+    event.preventDefault();
+    
+    const usernameEl = document.querySelector("#username-input-login");
+    const passwordEl = document.querySelector("#password-input-login");
+}

a6de6bdca1ce1171fa93b4a4263bfb156fb7ede2
jrsr5599 <johnraab2002@yahoo.com>
2023-09-07 16:09:35 -0700

    Update signup.js

---
+++ public/js/signup.js
@@ -0,0 +1,6 @@
+const signupFormHandler = async function(event) {
+    event.preventDefault();
+  
+    const usernameEl = document.querySelector("#username-input-signup");
+    const passwordEl = document.querySelector("#password-input-signup");
+}

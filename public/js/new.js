397b321036a7e0bb15e01592659fa0dbaf0912fe
jrsr5599 <johnraab2002@yahoo.com>
2023-09-07 16:08:57 -0700

    Update new.js

---
+++ public/js/new.js
@@ -0,0 +1,6 @@
+const newFormHandler = async function(event) {
+    event.preventDefault();
+  
+    const title = document.querySelector('input[name="post-title"]').value;
+    const post_content = document.querySelector('textarea[name="post-content"]').value;
+}

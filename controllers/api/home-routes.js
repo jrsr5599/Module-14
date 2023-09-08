06452c89e89f735b6aed83f06ebce3e914d131a7
jrsr5599 <johnraab2002@yahoo.com>
2023-09-08 09:43:39 -0700

    Update home-routes.js

--- controllers/api/home-routes.js
+++ controllers/api/home-routes.js
@@ -23,7 +23,7 @@ router.get('/', (req, res) => {
               include: [User],
             },
           ],
-        };
+     })
 
      .then((dbPostData) => {
         if (dbPostData) {
@@ -38,3 +38,23 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
       });
   });
+
+  router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+ 
+    res.render('login');
+  });
+ 
+  router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect ('/');
+      return;
+    }
+ 
+    res.render('signup');
+  });
+ 
+  module.exports = router;

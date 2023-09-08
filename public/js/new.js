faf9d29590d8c28ee9dffa9c6867eff04acf16bb
jrsr5599 <johnraab2002@yahoo.com>
2023-09-07 18:21:41 -0700

    Update new.js

--- public/js/new.js
+++ public/js/new.js
@@ -3,4 +3,19 @@ const newFormHandler = async function(event) {
   
     const title = document.querySelector('input[name="post-title"]').value;
     const post_content = document.querySelector('textarea[name="post-content"]').value;
-}
+
+    await fetch(`/api/post`, {
+        method: 'POST',
+        body: JSON.stringify({
+          title,
+          post_content
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+    
+       document.location.replace('/dashboard');
+    };
+    
+    document.querySelector('#new-post-form').addEventListener('submit', newFormHandler);   

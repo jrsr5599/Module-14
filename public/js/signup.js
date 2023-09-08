f395400d4ec4b8c12e6a96d812b7f43ac313780a
jrsr5599 <johnraab2002@yahoo.com>
2023-09-07 17:41:35 -0700

    Update signup.js

--- public/js/signup.js
+++ public/js/signup.js
@@ -4,3 +4,21 @@ const signupFormHandler = async function(event) {
     const usernameEl = document.querySelector("#username-input-signup");
     const passwordEl = document.querySelector("#password-input-signup");
 }
+
+if (body) {
+    await fetch('/api/comments', {
+      method: 'POST',
+      body: JSON.stringify({
+        postId,
+        body
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+      document.location.reload();
+    }
+  };
+
+document.querySelector('#new-comment-form').addEventListener('submit', commentFormHandler);

18cc723d24aaa58d5ed99f412d191cdf2affacc5
jrsr5599 <johnraab2002@yahoo.com>
2023-09-08 09:44:39 -0700

    Update dashboard-routes.js

--- controllers/api/dashboard-routes.js
+++ controllers/api/dashboard-routes.js
@@ -1,3 +1,10 @@
 const router = require('express').Router();
 const { Post } = require('../models');
 const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+      where: {
+        userId: req.session.userId
+      }
+     }

c10e1d461ddc9cc525742f45a99cc41d8b5a0d1e
jrsr5599 <johnraab2002@yahoo.com>
2023-09-08 09:40:21 -0700

    Update home-routes.js

--- controllers/api/home-routes.js
+++ controllers/api/home-routes.js
@@ -1,2 +1,15 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+         include: [User],
+       })
+         .then(dbPostData => {
+           const posts = dbPostData.map(post => post.get({ plain: true }));
+           res.render('all-posts', { posts });
+        })
+         .catch(err => {
+           res.status(500).json(err);
+         });
+     });
